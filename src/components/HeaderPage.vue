e23f60650ff20f3cee3fd495cdfdd08b946027e9
AngeloBosetti <150057418+AngeloBosetti@users.noreply.github.com>
2024-05-09 17:58:24 +0200

    debut de la page d acceuil+header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -2,7 +2,9 @@
 import { RouterView } from 'vue-router'
 </script>
 <template>
-  <header>
-    <h1>Header</h1>
+  <header class="flex items-center justify-between bg-slate-100 gap-16">
+    <img class="w-12 h-12" src="/public/Logo_MysClick.png" alt="">
+    <h3 class=" text-black font-semibold">MysClcik</h3>
+    <img class="w-12 h-12" src="/public/Logo_compte.png" alt="">
   </header>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import './assets/css/style.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import V3ScrollLock from 'v3-scroll-lock'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(V3ScrollLock, {})
+
 
 app.mount('#app')
